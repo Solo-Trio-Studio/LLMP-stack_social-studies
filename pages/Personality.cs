78ded0f6703de3b71e90cb2f04e026544a6ce72b
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:38:35 -0400

    Update and rename p.html to Personality.cs

---
+++ pages/Personality.cs
@@ -0,0 +1,11 @@
+using System; 
+namespace Personality { 
+    class Personality { 
+        static void Main(string[] args) {
+            Console.WriteLine("Personality");
+            Console.WriteLine("Tritypes are based on picking 2 (both)/3 (Revenue)/4 (Purchasing) & 8 (both)/9 (Administration)/1 (Management) & 5 (R & D)/6 (Production)/7 (both).");
+            Console.WriteLine("Interpretation: https://winterune.tumblr.com/post/152397510587/the-27-tritype-discriptions");
+            Console.ReadKey(); 
+        } 
+    } 
+}
