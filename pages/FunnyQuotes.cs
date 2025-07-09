0408a7ceaf263f182f5c3b1e6e41f08008339090
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:21:21 -0400

    Update and rename f.html to FunnyQuotes.cs

---
+++ pages/FunnyQuotes.cs
@@ -0,0 +1,13 @@
+using System; 
+namespace FunnyQuotes { 
+    class FunnyQuotes { 
+        static void Main(string[] args) {
+            Console.WriteLine("Funny Quotes"); 
+	    Console.WriteLine("Sla fa boom aw, onen finen, ja ju je jo, I followed a seafood diet but it didn't follow be back so I unfollowed it and don't eat food when I see it smh."); 
+            Console.ReadKey(); 
+        } 
+    } 
+}
+
+		
+	
