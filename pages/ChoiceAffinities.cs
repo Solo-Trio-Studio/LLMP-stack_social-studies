99d8a4e3fdbae9c9665780e352004b22c6e4986e
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:14:26 -0400

    Update and rename a.html to ChoiceAffinities.cs

---
+++ pages/ChoiceAffinities.cs
@@ -0,0 +1,10 @@
+using System; 
+namespace ChoiceAffinities { 
+    class ChoiceAffinities { 
+        static void Main(string[] args) {
+            Console.WriteLine("Affinities of Choice"); 
+	    Console.WriteLine("Leaders in the past could not make decisions because they had too little information. Leaders in the future will be unable to make decisions because they will have too much information."); 
+            Console.ReadKey(); 
+        } 
+    } 
+}
