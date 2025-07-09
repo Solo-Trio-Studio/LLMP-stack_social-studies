0250514851c840437fd65906b97f4489530b5336
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:15:53 -0400

    Update and rename c.html to ColorPallete.cs

---
+++ pages/ColorPallete.cs
@@ -0,0 +1,11 @@
+using System; 
+namespace ColorPallete { 
+    class ColorPallete { 
+        static void Main(string[] args) {
+	    const colors = ["red", "green", "blue", "cyan", "yellow", "magenta"]
+	    string palletes = ["yellow magenta", "cyan magenta", "cyan yellow", "green blue", "red blue", "red green"]
+            Console.WriteLine(""); 
+            Console.ReadKey(); 
+        } 
+    } 
+}
