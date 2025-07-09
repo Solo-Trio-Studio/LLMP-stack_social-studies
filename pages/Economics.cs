6768bce69f17edb07959c87dc04eb4bfba61f269
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:20:10 -0400

    Update and rename e.html to Economics.cs

---
+++ pages/Economics.cs
@@ -0,0 +1,10 @@
+using System; 
+namespace Economics { 
+    class Economics { 
+        static void Main(string[] args) {
+            Console.WriteLine("Economics"); 
+	    Console.WriteLine("Microeconomics, Macroeconomics"); 
+            Console.ReadKey(); 
+        } 
+    } 
+}
